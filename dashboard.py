d59ebef984437f264bf0156f9517d1e594f9f3f9
KelvinSousaDev <kelvinsousa.dev@gmail.com>
2025-12-16 06:47:24 -0300

    Feat: Dashboard interativo com filtro de produtos e KPIs dinamicos

--- dashboard.py
+++ dashboard.py
@@ -16,28 +16,32 @@ dados = carregar_dados()
 
 if dados:
   df = pd.DataFrame(dados)
-
   df['data_hora'] = pd.to_datetime(df['data_hora'])
   df['valor'] = pd.to_numeric(df['valor'])
 
+  lista_opcoes = df['produto'].unique()
+  opcao_escolhida = st.sidebar.selectbox("Escolha o Produto", lista_opcoes)
+
+  df_filtrado = df[df['produto'] == opcao_escolhida]
+
   col1, col2, col3 = st.columns(3)
 
-  atual = df['valor'].iloc[-1]
-  minimo = df['valor'].min()
-  media = df['valor'].mean()
+  atual = df_filtrado['valor'].iloc[-1]
+  minimo = df_filtrado['valor'].min()
+  media = df_filtrado['valor'].mean()
 
   variacao = 0
 
-  if df['valor'].count() >= 2:
-    anterior = df['valor'].iloc[-2]
+  if df_filtrado['valor'].count() >= 2:
+    anterior = df_filtrado['valor'].iloc[-2]
     variacao = atual - anterior
 
   col1.metric("Preço Atual", f"R$ {atual:.2f}", delta=f"{variacao:.2f}", delta_color="inverse")
   col2.metric("Preço Minimo", f"R$ {minimo:.2f}")
   col3.metric("Preço Médio", f"R$ {media:.2f}")
 
-  st.subheader("Evolução do Preço")
-  st.line_chart(df, x="data_hora", y="valor")
+  st.subheader(f"Evolução do Preço {opcao_escolhida}")
+  st.line_chart(df_filtrado, x="data_hora", y="valor")
 
   with st.expander("Ver Dados Brutos"):
         st.dataframe(df.sort_values(by="data_hora", ascending=False))
--- executar_ronda.bat
+++ executar_ronda.bat
@@ -1,3 +1,4 @@
 @echo off
 cd /d "C:\Users\Kelvin\Desktop\Python\VigilantePrecos"
+call venv\Scripts\activate
 python vigilante.py
